10d6e80313632f3653544decf01a095bee42f299
Brad Copenhaver and daniel munger <brad@>
2016-10-25 11:37:47 -0700

    create survey to predict fortune using checkboxes and arrays

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+var unlucky = []
+var lucky = []
+var dead = []
+
+var Fortune = function(){
+  if(unlucky.length > lucky.length && unlucky.length > dead.length) {
+    return ".unlucky"
+  }
+  else if(lucky.length > unlucky.length && lucky.length > dead.length) {
+    return ".lucky"
+  }
+  else if(unlucky.length === lucky.length && dead.length === 0){
+    return ".boring"
+  }
+  else{
+    return".dead"
+  };
+};
+
+
+//
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    $(".unlucky").hide();
+    $(".lucky").hide();
+    $(".dead").hide();
+    $(".boring").hide();
+    $("input:checkbox[name=unlucky-events]:checked")
+    .each(function() {
+        var unluckyResponses = $(this).val();
+        unlucky.push(unluckyResponses);
+    });
+    $("input:checkbox[name=lucky-events]:checked")
+    .each(function() {
+        var luckyResponses = $(this).val();
+        lucky.push(luckyResponses);
+    });
+    $("input:checkbox[name=horrible-events]:checked")
+    .each(function() {
+        var horribleResponses = $(this).val();
+        dead.push(horribleResponses);
+    });
+
+    $(".output").show();
+    $(Fortune()).show();
+
+  });
+});
